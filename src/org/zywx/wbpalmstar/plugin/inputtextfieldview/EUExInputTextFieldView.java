ac60ac2ee37024e4cadc738e696aed409f6b23e5
appcanyajun <yajun.duan@3g2win.com>
2016-01-29 18:52:37 +0800

    添加接口，添加自定义参数，相关代码优化

--- src/org/zywx/wbpalmstar/plugin/inputtextfieldview/EUExInputTextFieldView.java
+++ src/org/zywx/wbpalmstar/plugin/inputtextfieldview/EUExInputTextFieldView.java
@@ -1,37 +1,101 @@
+/*
+ *  Copyright (C) 2014 The AppCan Open Source Project.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.zywx.wbpalmstar.plugin.inputtextfieldview;
+
+import org.json.JSONObject;
+import org.zywx.wbpalmstar.engine.EBrowserView;
+import org.zywx.wbpalmstar.engine.universalex.EUExBase;
+import org.zywx.wbpalmstar.engine.universalex.EUExUtil;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Message;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
-import org.json.JSONObject;
-import org.zywx.wbpalmstar.engine.EBrowserView;
-import org.zywx.wbpalmstar.engine.universalex.EUExBase;
-
 public class EUExInputTextFieldView extends EUExBase{
 
-    public static final String INPUTTEXTFIELDVIEW_FUN_PARAMS_KEY = "inputtextfieldviewFunParamsKey";
-    public static final String INPUTTEXTFIELDVIEW_ACTIVITY_ID = "inputtextfieldviewActivityID";
+	private static final String TAG = "uexInputTextFieldView";
+	private static final String INPUTTEXTFIELDVIEW_FUN_PARAMS_KEY = "inputTextFieldViewFunParamsKey";
 
-    public static final int INPUTTEXTFIELDVIEW_MSG_OPEN = 0;
-    public static final int INPUTTEXTFIELDVIEW_MSG_CLOSE = 1;
-    private static final String BUNDLE_DATA = "data";
-    private static final int MSG_SET_INPUT_FOCUSED = 2;
-    private static final String TAG = "ACEInputTextFieldViewFragment";
-    private ACEInputTextFieldViewFragment inputTextFieldViewFragment;
+	private static final int INPUTTEXTFIELDVIEW_MSG_OPEN = 0;
+	private static final int INPUTTEXTFIELDVIEW_MSG_CLOSE = 1;
+	private static final int INPUTTEXTFIELDVIEW_MSG_SET_INPUT_FOCUSED = 2;
+	private static final int INPUTTEXTFIELDVIEW_MSG_HIDE_KEYBOARD = 3;
+	private static final int INPUTTEXTFIELDVIEW_MSG_GET_INPUTBAR_HEIGHT = 4;
+    
+    private ACEInputTextFieldView inputTextFieldView;
 
     public EUExInputTextFieldView(Context context, EBrowserView view) {
         super(context, view);
     }
 
+    @Override
+	public void onHandleMessage(Message message) {
+		if (message == null) {
+			return;
+		}
+		switch (message.what) {
+		case INPUTTEXTFIELDVIEW_MSG_OPEN:
+			handleOpen(message);
+			break;
+		case INPUTTEXTFIELDVIEW_MSG_CLOSE:
+			handleClose();
+			break;
+		case INPUTTEXTFIELDVIEW_MSG_SET_INPUT_FOCUSED:
+			handleSetInputFocusedMsg();
+			break;
+		case INPUTTEXTFIELDVIEW_MSG_HIDE_KEYBOARD:
+			handleHideKeyboard();
+			break;
+		case INPUTTEXTFIELDVIEW_MSG_GET_INPUTBAR_HEIGHT:
+			handleGetInputBarHeight();
+			break;
+		default:
+			;
+		}
+	}
+
+    public void open(String[] params) {
+        sendMessageWithType(INPUTTEXTFIELDVIEW_MSG_OPEN, params);
+    }
+
+    public void close(String[] params) {
+        sendMessageWithType(INPUTTEXTFIELDVIEW_MSG_CLOSE, params);
+    }
+    
+    public void setInputFocused(String[] params) {
+    	sendMessageWithType(INPUTTEXTFIELDVIEW_MSG_SET_INPUT_FOCUSED,params);
+    }
+    
+    public void hideKeyboard(String[] params) {
+    	sendMessageWithType(INPUTTEXTFIELDVIEW_MSG_HIDE_KEYBOARD,params);
+    }
+    
+    public void getInputBarHeight(String[] params) {
+    	sendMessageWithType(INPUTTEXTFIELDVIEW_MSG_GET_INPUTBAR_HEIGHT,params);
+    }
+    
     private void sendMessageWithType(int msgType, String[] params) {
         if (mHandler == null) {
             return;
@@ -44,134 +108,83 @@ public class EUExInputTextFieldView extends EUExBase{
         msg.setData(b);
         mHandler.sendMessage(msg);
     }
-
-    public void setInputFocused(String[] params) {
-        Message msg = new Message();
-        msg.obj = this;
-        msg.what = MSG_SET_INPUT_FOCUSED;
-        Bundle bd = new Bundle();
-        bd.putStringArray(BUNDLE_DATA, params);
-        msg.setData(bd);
-        mHandler.sendMessage(msg);
-    }
-
-    private void setInputFocusedMsg() {
-        if (inputTextFieldViewFragment != null) {
-            inputTextFieldViewFragment.setInputFocused();
-        }
-    }
-
-    @Override
-    public void onHandleMessage(Message message) {
-        if(message == null){
-            return;
-        }
-        Bundle bundle=message.getData();
-        switch (message.what) {
-
-            case INPUTTEXTFIELDVIEW_MSG_OPEN:
-                handleOpen(message);
-                break;
-            case INPUTTEXTFIELDVIEW_MSG_CLOSE:
-                handleMessageInputTextFieldView(message);
-                break;
-            case MSG_SET_INPUT_FOCUSED:
-                setInputFocusedMsg();
-                break;
-            default:
-                super.onHandleMessage(message);
-        }
-    }
-
-    private void handleMessageInputTextFieldView(Message msg) {
-        if (inputTextFieldViewFragment != null) {
-            switch (msg.what) {
-                case INPUTTEXTFIELDVIEW_MSG_CLOSE:
-                    handleClose();
-                    break;
-            }
-        }
-    }
-
-    private void handleClose() {
-        if (inputTextFieldViewFragment != null){
-            removeFragmentFromWindow(inputTextFieldViewFragment);
-            inputTextFieldViewFragment.onDestroy();
-            inputTextFieldViewFragment = null;
-        }
-    }
-
+    
     private void handleOpen(Message msg) {
         String[] params = msg.getData().getStringArray(
                 INPUTTEXTFIELDVIEW_FUN_PARAMS_KEY);
         if (params == null || params.length < 1) return;
         try {
+            if (inputTextFieldView != null) return;
             JSONObject json = new JSONObject(params[0]);
-            String emojicons = json
-                    .getString(EInputTextFieldViewUtils.INPUTTEXTFIELDVIEW_PARAMS_JSON_KEY_EMOJICONS);
+            inputTextFieldView = new ACEInputTextFieldView(mContext,json,this);
 
-            String activityId = INPUTTEXTFIELDVIEW_ACTIVITY_ID
-                    + EUExInputTextFieldView.this.hashCode();
-            if (inputTextFieldViewFragment != null) return;
-            inputTextFieldViewFragment = new ACEInputTextFieldViewFragment();
-            inputTextFieldViewFragment.setUexBaseObj(this);
-            inputTextFieldViewFragment.setEmojiconswgtResXmlPath(emojicons);
-
-            boolean hasPlacehold = json
-                    .has(EInputTextFieldViewUtils.INPUTTEXTFIELDVIEW_PARAMS_JSON_KEY_PLACEHOLD);
-            if (hasPlacehold) {
-                String placehold = json
-                        .getString(EInputTextFieldViewUtils.INPUTTEXTFIELDVIEW_PARAMS_JSON_KEY_PLACEHOLD);
-                inputTextFieldViewFragment.setHint(placehold);
-            }
-            if (json.has(EInputTextFieldViewUtils
-                    .INPUTTEXTFIELDVIEW_PARAMS_JSON_KEY_BTN_COLOR)) {
-                inputTextFieldViewFragment.setBtnColor(json.getString(EInputTextFieldViewUtils.
-                        INPUTTEXTFIELDVIEW_PARAMS_JSON_KEY_BTN_COLOR));
-            }
-            if (json.has(EInputTextFieldViewUtils
-                    .INPUTTEXTFIELDVIEW_PARAMS_JSON_KEY_BTN_TEXT_COLOR)) {
-                inputTextFieldViewFragment.setBtnTextColor(json.getString(EInputTextFieldViewUtils.
-                        INPUTTEXTFIELDVIEW_PARAMS_JSON_KEY_BTN_TEXT_COLOR));
-            }
             DisplayMetrics dm = mContext.getResources().getDisplayMetrics();
             RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
-                    dm.widthPixels, RelativeLayout.LayoutParams.WRAP_CONTENT);
-            lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-            addFragmentToCurrentWindow(inputTextFieldViewFragment, lp, TAG);
-            if (inputTextFieldViewFragment.getView() != null){
-                mBrwView.setOnTouchListener(new OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View v, MotionEvent event) {
-                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                            DisplayMetrics dm = mContext.getResources().getDisplayMetrics();
-                            float h = inputTextFieldViewFragment.getView().getHeight();
-                            float y = event.getY();
-                            if (dm.heightPixels - Math.abs(y) > h) {
-                                if (inputTextFieldViewFragment != null) {
-                                    inputTextFieldViewFragment.outOfViewTouch();
-                                }
-                            }
-                        }
-                        return false;
-                    }
-                });
-            }
+                    dm.widthPixels, RelativeLayout.LayoutParams.MATCH_PARENT);
+            //lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            //lp.bottomMargin = json.optInt("bottom",0);
+            addView2CurrentWindow(inputTextFieldView, lp);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    
+	private void handleClose() {
+		if (inputTextFieldView == null) {
+			return;
+		}
+		removeViewFromCurrentWindow(inputTextFieldView);
+		inputTextFieldView.onDestroy();
+		inputTextFieldView = null;
+	}
 
-    public void open(String[] params) {
-        sendMessageWithType(INPUTTEXTFIELDVIEW_MSG_OPEN, params);
+    private void handleSetInputFocusedMsg() {
+        if (inputTextFieldView != null) {
+            inputTextFieldView.setInputFocused();
+        }
     }
-
-    public void close(String[] params) {
-        sendMessageWithType(INPUTTEXTFIELDVIEW_MSG_CLOSE, params);
-    }
+    
+    /**
+     * 隐藏键盘的接口
+     * @param params
+     */
+	private void handleHideKeyboard() {
+		if (inputTextFieldView != null) {
+			inputTextFieldView.outOfViewTouch();
+		}
+	}
+	
+	private void handleGetInputBarHeight() {
+		// 当前输入框的高度是固定的，50dp
+		int height = EUExUtil.dipToPixels(50);
+		String result = "{\"height\":" + "\"" + height + "\"}";
+		String jsCallBack = SCRIPT_HEADER
+				+ "if("
+				+ EInputTextFieldViewUtils.INPUTTEXTFIELDVIEW_FUN_CB_GET_INPUTBAR_HEIGHT
+				+ "){"
+				+ EInputTextFieldViewUtils.INPUTTEXTFIELDVIEW_FUN_CB_GET_INPUTBAR_HEIGHT
+				+ "('" + result + "');}";
+		onCallback(jsCallBack);
+	}
+    
+    private void addView2CurrentWindow(View child,
+			RelativeLayout.LayoutParams parms) {
+		int l = (int) (parms.leftMargin);
+		int t = (int) (parms.topMargin);
+		int w = parms.width;
+		int h = parms.height;
+		FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(w, h);
+		lp.gravity = Gravity.BOTTOM;
+		lp.leftMargin = l;
+		lp.bottomMargin = parms.bottomMargin;
+		lp.topMargin = t;
+		adptLayoutParams(parms, lp);
+		mBrwView.addViewToCurrentWindow(child, lp);
+	}
 
     @Override
     protected boolean clean() {
+    	Log.i(TAG, "clean");
         close(null);
         return false;
     }
